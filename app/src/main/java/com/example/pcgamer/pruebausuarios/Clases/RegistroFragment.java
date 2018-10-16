6f112f5f06fcbaa7914bcda0554ccaf7938dcb99
VezMar <44082117+VezMar@users.noreply.github.com>
2018-10-16 17:01:07 -0500

    Elegir Foto

--- app/src/main/java/com/example/pcgamer/pruebausuarios/Clases/RegistroFragment.java
+++ app/src/main/java/com/example/pcgamer/pruebausuarios/Clases/RegistroFragment.java
@@ -1,15 +1,26 @@
 package com.example.pcgamer.pruebausuarios.Clases;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -21,6 +32,8 @@ import com.android.volley.toolbox.Volley;
 import com.example.pcgamer.pruebausuarios.R;
 
 import org.json.JSONObject;
+
+import java.io.File;
 
 
 /**
@@ -37,6 +50,17 @@ public class RegistroFragment extends Fragment implements Response.Listener<JSON
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private static final int COD_SELECCIONA = 10;
+    private static final int COD_FOTO = 20;
+
+    private static final String CARPETA_PRINCIPAL= "MisImagenesApp/";
+    private static final String CARPETA_IMAGEN = "imagenes";
+    private static final String DIRECTORIO_IMAGEN = CARPETA_PRINCIPAL + CARPETA_IMAGEN;
+
+    private String path;
+    File fileImagen;
+    Bitmap bitmap;
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -44,8 +68,12 @@ public class RegistroFragment extends Fragment implements Response.Listener<JSON
     private OnFragmentInteractionListener mListener;
 
     EditText CampoNombre, CampoDireccion, CampoImagen;
-    Button btnRegistro;
+    Button btnRegistro, btnFoto;
+    ImageView imgFoto;
+
     ProgressDialog progreso;
+
+
 
     RequestQueue request;
     JsonObjectRequest jsonObjectRequest;
@@ -88,16 +116,23 @@ public class RegistroFragment extends Fragment implements Response.Listener<JSON
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View vista=inflater.inflate(R.layout.fragment_registro, container, false);
+        View vista = inflater.inflate(R.layout.fragment_registro, container, false);
         CampoNombre = (EditText) vista.findViewById(R.id.CampoNombre);
         CampoDireccion = (EditText) vista.findViewById(R.id.CampoDireccion);
         //CampoImagen = (EditText) vista.findViewById(R.id.CampoImagen);
 
         btnRegistro = (Button) vista.findViewById(R.id.btnRegistrar);
+        btnFoto = (Button) vista.findViewById(R.id.btnFoto);
+
+        imgFoto = (ImageView) vista.findViewById(R.id.imgFoto);
+
+
 
         String url;
 
         request = Volley.newRequestQueue(getContext());
+
+
 
         btnRegistro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -106,9 +141,96 @@ public class RegistroFragment extends Fragment implements Response.Listener<JSON
             }
         });
 
+        btnFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDialogOpciones();
+            }
+        });
+
 
         //inflater.inflate(R.layout.fragment_registro, container, false
         return vista;
+    }
+
+    private void mostrarDialogOpciones() {
+        final CharSequence [] opciones ={"Elegir de galeria","Cacelar"};
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Elige una opcion");
+        builder.setItems(opciones, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (opciones[i].equals("Tomar foto")){
+
+                    abrirCamara();
+
+
+                }else{
+                    if (opciones[i].equals("Elegir de galeria")){
+                        Intent intent = new Intent(Intent.ACTION_PICK,
+                                MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("image/");
+                        startActivityForResult(intent.createChooser(intent,"Seleccione"),COD_SELECCIONA);
+
+                    }else {
+                        dialogInterface.dismiss();
+                    }
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void abrirCamara() {
+        File miFile = new File(Environment.getExternalStorageDirectory(),DIRECTORIO_IMAGEN);
+        boolean isCreada=miFile.exists();
+
+        if (isCreada==false){
+            isCreada=miFile.mkdirs();
+        }
+
+        if (isCreada==true){
+            Long consecutivo = System.currentTimeMillis()/1000;
+            String nombre=consecutivo.toString()+".jpg";
+
+            path=Environment.getExternalStorageDirectory()+File.separator+DIRECTORIO_IMAGEN
+                    +File.separator+nombre;
+
+            fileImagen= new File(path);
+
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(fileImagen));
+
+            startActivityForResult(intent, COD_FOTO);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode){
+            case COD_SELECCIONA:
+                Uri miPath=data.getData();
+                imgFoto.setImageURI(miPath);
+                break;
+
+            case COD_FOTO:
+                Toast.makeText(getContext(), "Tomar foto",Toast.LENGTH_SHORT).show();
+                MediaScannerConnection.scanFile(getContext(), new String[]{path}, null,
+                        new MediaScannerConnection.OnScanCompletedListener() {
+                            @Override
+                            public void onScanCompleted(String s, Uri uri) {
+                                Log.i("Path",""+path);
+                            }
+                        });
+
+                bitmap = BitmapFactory.decodeFile(path);
+                imgFoto.setImageBitmap(bitmap);
+
+                break;
+
+        }
     }
 
     private void cargarWebService() {
